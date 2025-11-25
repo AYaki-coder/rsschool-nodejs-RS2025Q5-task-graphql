6cc2825c5da0ae0d951dd9bc7b1fffdf19c1fa89
ayaki-coder <sj.a.yakimovich@gmail.com>
2025-11-25 18:40:23 +0300

    feat: add prime loader

--- src/routes/graphql/resolvers/user.ts
+++ src/routes/graphql/resolvers/user.ts
@@ -1,12 +1,74 @@
-import { GraphQLList, GraphQLString } from 'graphql';
+import { GraphQLList, GraphQLResolveInfo, GraphQLString } from 'graphql';
 import { ChangeUserInput, CreateUserInput, userType } from '../types/user.js';
-import { MyContext } from '../types/common.js';
+import { MyContext, UserInfo, UserInfoExtended, UserSubsInfo } from '../types/common.js';
 import { UUIDType } from '../types/uuid.js';
+import {
+  parseResolveInfo,
+  ResolveTree,
+  simplifyParsedResolveInfoFragmentWithType,
+} from 'graphql-parse-resolve-info';
 
 export const getALLUsers = {
   type: new GraphQLList(userType),
-  resolve: async (_parent, _arg, _context: MyContext) => {
-    return _context.prisma.user.findMany();
+  resolve: async (
+    _parent,
+    _arg,
+    _context: MyContext,
+    resolveInfo: GraphQLResolveInfo,
+  ) => {
+    const parsedResolveInfoFragment = parseResolveInfo(resolveInfo) as ResolveTree;
+    const { fields }: { fields: Record<string, unknown> } =
+      simplifyParsedResolveInfoFragmentWithType(
+        parsedResolveInfoFragment,
+        resolveInfo.returnType,
+      );
+    const include: Record<string, unknown> = {};
+    if (fields['userSubscribedTo']) {
+      include.userSubscribedTo = true;
+    }
+    if (fields['subscribedToUser']) {
+      include.subscribedToUser = true;
+    }
+
+    if (Object.entries(include).length === 0) {
+      return _context.prisma.user.findMany();
+    }
+
+    const users: UserInfoExtended[] = await _context.prisma.user.findMany({ include });
+
+    const dataUsers = new Map(
+      users.map((user) => [
+        user.id,
+        { name: user.name, balance: user.balance, id: user.id },
+      ]),
+    );
+    users.forEach(({ id, subscribedToUser, userSubscribedTo }) => {
+      const authors: UserInfo[] = [];
+      const subscribers: UserInfo[] = [];
+
+      if (userSubscribedTo && userSubscribedTo.length) {
+        userSubscribedTo.forEach(({ authorId }: UserSubsInfo) => {
+          const author = dataUsers.get(authorId);
+          if (author) {
+            authors.push(author);
+          }
+        });
+      }
+
+      if (subscribedToUser && subscribedToUser.length) {
+        subscribedToUser.forEach(({ subscriberId }: UserSubsInfo) => {
+          const subscriber = dataUsers.get(subscriberId);
+          if (subscriber) {
+            subscribers.push(subscriber);
+          }
+        });
+      }
+
+      _context.loader.subscribedToUser.prime(id, subscribers);
+      _context.loader.userSubscribedTo.prime(id, authors);
+    });
+
+    return users;
   },
 };
 
--- src/routes/graphql/types/common.ts
+++ src/routes/graphql/types/common.ts
@@ -5,3 +5,18 @@ export interface MyContext {
   prisma: PrismaClient;
   loader: Loader;
 }
+
+export interface UserInfo {
+  id: string;
+  name: string;
+  balance: number;
+}
+
+export interface UserSubsInfo {
+  authorId: string;
+  subscriberId: string;
+}
+export interface UserInfoExtended extends UserInfo {
+  subscribedToUser?: Array<UserSubsInfo>;
+  userSubscribedTo?: Array<UserSubsInfo>;
+}
